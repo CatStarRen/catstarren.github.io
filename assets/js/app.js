dc2effdfa6ce01c1f1ca8de459aea227ef5c215f
inferjay <inferjay@gmail.com>
2017-10-31 23:50:49 +0800

    Add CNAME file and modify app.js file author info

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,8 @@
 /*
-* @Author: Inferjay
+* @Author: inferjay
 * @Date:   2017-10-30 21:32:41
-* @Last Modified by:   ZHC
-* @Last Modified time: 2017-10-30 21:37:19
+* @Last Modified by:   inferjay
+* @Last Modified time: 2017-10-31 15:49:28
 */
 var wechatQrcode = $('.follow-us-wechat-official-qrcode');
 $('.follow-us-social-item.wechat').hover(function() {
